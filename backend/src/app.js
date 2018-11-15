3d497e5e8564f4bad4a0fac0e5bcf19eee2d9a0c
Abhayprad Jha <abhaypradjha@gmail.com>
2018-11-15 16:21:48 -0500

    Created basic server

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,3 +3,14 @@ const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
+const errorhandler = require('errorhandler');
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(morgan('short'));
+app.use(errorhandler());
+app.use(helmet());
+
+app.listen(port, console.log("Hello World"));
+
