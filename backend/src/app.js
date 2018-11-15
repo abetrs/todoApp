11f738332c1eb28e1e77ce47f0b936926ba2ef40
Abhayprad Jha <abhaypradjha@gmail.com>
2018-11-15 17:11:44 -0500

    Created todo GET POST and GET by id routes still need to do DELETE. Day 1 end

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,6 +10,8 @@ const errorhandler = require('errorhandler');
 // Setting port and IP
 const port = process.env.PORT || 5000;
 const url = process.env.URL || 'localhost';
+// Connect to mongoose
+mongoose.connect('mongodb://127.0.0.1:27017', console.log('Connected to mongo'));
 // Middlewares
 app.use(cors());
 app.use(morgan('short'));
@@ -18,6 +20,6 @@ app.use(helmet());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 // Routes
-app.use(todoRoutes);
+app.use('/todos', todoRoutes);
 // Listening on http://localhost:5000
 app.listen(port, url, console.log(`http://${url}:${port}`));
--- backend/src/todos/todo.js
+++ backend/src/todos/todo.js
@@ -1,20 +1,40 @@
 const express = require('express');
 const router = express.Router();
+const Todo = require('./todo-model');
 
 router.get('/getTodos', (req, res, next) => {
-    res.send({
-        message: "These are all the todos"
-    })
+    Todo.find()
+    .select('_id title body')
+    .exec()
+    .then(docs => {
+        res.send(docs);
+    }).catch(err => {
+        error: err
+    });
 });
 router.get('/getTodos/:todoId', (req, res, next) => {
-    res.send({
-        message: "This is a specific todo with the id: " + req.params.todoId
+    Todo.findById({_id: req.params.todoId })
+    .select('title body')
+    .exec()
+    .then(docs => {
+        res.send(docs);
+    }).catch(err => {
+        error: 'Not found'
     });
 });
 
 router.post('/newTodo', (req, res, next) => {
-    res.send({
-        "This is the data you sent": req.body
+    const newTodo = new Todo({
+        title: req.body.title,
+        body: req.body.body,
+    });
+    newTodo.save()
+    .then(_ => {
+        res.send({
+            message: "Success"
+        });
+    }).catch(err => {
+        message: new Error('You are missing something in the body');
     });
 });
 
