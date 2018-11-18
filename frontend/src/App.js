a9f23e31a55fb4c28066d121d558b7cbfe7e51e2
Abhayprad Jha <abhaypradjha@gmail.com>
2018-11-18 17:14:46 -0500

    Removed defaults in react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World!</h1>
+        <h1>Welcome to the todoApp</h1>
       </div>
     );
   }
